8fd451d1b5182c4bd28682ff1f9ccd1952e3a8b1
David Fernandez <david.fernandez@>
2014-05-21 17:22:29 -0400

    woo

---
+++ js/main.js
@@ -0,0 +1,21 @@
+(function(){
+    var menu = document.querySelectorAll('nav')[0],
+        burger = document.querySelectorAll('header .burger')[0],
+        main = document.getElementById('mainWrap');
+
+    menu.addEventListener('click', function(e){
+        var sib = e.target.nextElementSibling;
+        console.log(sib);
+
+        if(sib){
+            sib.classList.toggle('expanded');
+        }
+    });
+
+    burger.addEventListener('click', function(e){
+        main.classList.toggle('shifted')
+    })
+
+
+
+})();
