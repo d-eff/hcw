a143ce0955a2840d21cf22f15d6bd5b6279006b5
David F <dfernandez06@gmail.com>
2014-05-28 15:52:29 -0400

    fix menu

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@
     var menu = document.querySelectorAll('.parent_link'),
         burger = document.querySelectorAll('header .burger')[0],
         main = document.getElementById('mainWrap'),
-        nav = document.getElementById('navWrap');
+        nav = document.getElementById('navWrap'),
+        foot = document.getElementById("main_footer");
 
     for(var x = 0; x < menu.length; ++x){
         (function (x){
@@ -28,7 +29,7 @@
       if(window.matchMedia('screen and (max-width:800px)').matches){ 
         window.onscroll = function(){
             console.log("scroll: " + window.scrollY + " nav: " + nav.offsetHeight + " main: " + main.offsetHeight);
-          if(( window.scrollY + nav.offsetHeight ) > main.offsetHeight){
+          if(( window.scrollY + nav.offsetHeight ) > (main.offsetHeight + foot.offsetHeight)){
               console.log("pop");
             nav.style.position = "absolute";    
             nav.style.bottom = "95px";
